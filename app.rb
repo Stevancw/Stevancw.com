37f260f5b53a0173f613b7ae19e9fcff35a7ba8a
Stevan C Wing <stevancw@me.com>
2013-12-27 18:13:06 +0000

    Enabled catagories and added new blog post

--- app.rb
+++ app.rb
@@ -27,43 +27,11 @@ module Nesta
         Nesta::Page.find_articles[0..count - 1]
       end
 
-      
-    #Trying to list by metadata (delete if you figure it out)
-      # def my_category_pages
-      #   Page.find_all.select { |p| p.flagged_as?('shown-on-blog') }
-      # end
-
-      def my_pages
-        Page.find_all.select { |p| p.flagged_as?('medicine') }
-      end
-
-      def pages
-        in_category = Page.find_all.select do |page|
-        page.date.nil? && page.categories.include?(self)
-      end
-        in_category.sort do |x, y|
-        by_priority = y.priority(path) <=> x.priority(path)
-          if by_priority == 0
-            x.link_text.downcase <=> y.link_text.downcase
-          else
-            by_priority
-          end
+     private
+        def category_strings
+          strings = metadata('categories')
+          strings.nil? ? [] : strings.split(',').map { |string| string.strip }
         end
-      end
-
-
-      def categories
-        paths = category_strings.map { |specifier| specifier.sub(/:-?\d+$/, '') }
-        pages = valid_paths(paths).map { |p| Page.find_by_path(p) }
-        pages.sort do |x, y|
-        x.link_text.downcase <=> y.link_text.downcase
-        end
-      end
-
-    #   def my_pages
-    #     Page.find_all.select do |page|
-    #       page.category
-    #   end
     end
 
     # Add new routes here.
